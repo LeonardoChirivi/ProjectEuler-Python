4f5c964cfd0146dd55ee35d96218e177b95b626a
Leonardo Chirivì <leonardochirivi1@gmail.com>
2015-12-27 16:57:30 +0100

    Project Euler Python solutions

--- projectEuler/euler10SummationOfPrimes.py
+++ projectEuler/euler10SummationOfPrimes.py
@@ -8,18 +8,18 @@ t = time.time()
 
 sieve = [True]*2000000
 sieve[0] = False
-primes = []
+primes = 0
 
 for i in xrange( 2, 2*10**6 ):
 
     if sieve[i-1]:
 
-        primes.append(i)
+        primes += i
 
         for j in xrange( i*i, 2*10**6, i ):
 
             sieve[j-1] = False
 
             
-print sum(primes)
+print primes
 print 'Time elapsed:', time.time() - t
